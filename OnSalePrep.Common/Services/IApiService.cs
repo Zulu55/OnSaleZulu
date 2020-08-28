ecdde996d3d1257377b8a7185d2e0728c3c8a079
Juan Carlos Zuluaga <Juan.Zuluaga@rockwellautomation.com>
2020-08-27 20:49:21 -0500

    Put a comment to show how is the commit on mac

--- OnSalePrep.Common/Services/IApiService.cs
+++ OnSalePrep.Common/Services/IApiService.cs
@@ -6,6 +6,14 @@ namespace OnSalePrep.Common.Services
 {
     public interface IApiService
     {
+        /// <summary>
+        /// Get a generic list 
+        /// </summary>
+        /// <typeparam name="T">Is the class</typeparam>
+        /// <param name="urlBase">The user base</param>
+        /// <param name="servicePrefix">The service prefix</param>
+        /// <param name="controller">The controller name</param>
+        /// <returns></returns>
         Task<Response> GetListAsync<T>(string urlBase, string servicePrefix, string controller);
 
         Task<Response> GetTokenAsync(string urlBase, string servicePrefix, string controller, TokenRequest request);
