d6171f43eca3e7ff6573dbc8a01db35eeb224c2b
Juan Carlos Zuluaga <Juan.Zuluaga@rockwellautomation.com>
2020-08-30 10:41:21 -0500

    Add products to cart

--- OnSalePrep.Prism/OnSalePrep.Prism/ViewModels/AddToCartPageViewModel.cs
+++ OnSalePrep.Prism/OnSalePrep.Prism/ViewModels/AddToCartPageViewModel.cs
@@ -1,25 +1,38 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 using OnSalePrep.Common.Entities;
+using OnSalePrep.Common.Helpers;
+using OnSalePrep.Common.Models;
 using OnSalePrep.Common.Responses;
 using OnSalePrep.Prism.Helpers;
+using OnSalePrep.Prism.Views;
+using Prism.Commands;
 using Prism.Navigation;
 
 namespace OnSalePrep.Prism.ViewModels
 {
     public class AddToCartPageViewModel : ViewModelBase
     {
+        private readonly INavigationService _navigationService;
         private ProductResponse _product;
         private ObservableCollection<ProductImage> _images;
         private bool _isRunning;
         private bool _isEnabled;
+        private DelegateCommand _addToCartCommand;
 
         public AddToCartPageViewModel(INavigationService navigationService)
             : base(navigationService)
         {
+            _navigationService = navigationService;
             Title = Languages.AddToCart;
             IsEnabled = true;
             Quantity = 1;
         }
+
+        public DelegateCommand AddToCartCommand => _addToCartCommand ?? (_addToCartCommand = new DelegateCommand(AddToCartAsync));
 
         public float Quantity { get; set; }
 
@@ -58,6 +71,43 @@ namespace OnSalePrep.Prism.ViewModels
                 Product = parameters.GetValue<ProductResponse>("product");
                 Images = new ObservableCollection<ProductImage>(Product.ProductImages);
             }
+        }
+
+        private async void AddToCartAsync()
+        {
+            bool isValid = await ValidateDataAsync();
+            if (!isValid)
+                {
+                return;
+            }
+
+            List<OrderDetail> orderDetails = JsonConvert.DeserializeObject<List<OrderDetail>>(Settings.OrderDetails);
+            if (orderDetails == null)
+            {
+                orderDetails = new List<OrderDetail>();
+            }
+
+            orderDetails.Add(new OrderDetail
+            {
+                Product = Product,
+                Quantity = Quantity,
+                Remarks = Remarks
+            });
+
+            Settings.OrderDetails = JsonConvert.SerializeObject(orderDetails);
+            await App.Current.MainPage.DisplayAlert(Languages.Error, Languages.AddToCartMessage, Languages.Accept);
+            await _navigationService.NavigateAsync($"/{nameof(OnSaleMasterDetailPage)}/NavigationPage/{nameof(ProductsPage)}");
+        }
+
+        private async Task<bool> ValidateDataAsync()
+        {
+            if (Quantity == 0)
+            {
+                await App.Current.MainPage.DisplayAlert(Languages.Error, Languages.QuantityError, Languages.Accept);
+                return false;
+            }
+
+            return true;
         }
     }
 }
