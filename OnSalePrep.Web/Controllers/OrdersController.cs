7b9476ee8c2da9bd661a695a89dfb9e0c5cad963
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2020-09-09 22:25:15 -0500

    Index in orders

---
+++ OnSalePrep.Web/Controllers/OrdersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnSalePrep.Web.Data;
+using System.Threading.Tasks;
+
+namespace OnSalePrep.Web.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class OrdersController : Controller
+    {
+        private readonly DataContext _context;
+
+        public OrdersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Orders
+                .Include(p => p.User)
+                .Include(p => p.OrderDetails)
+                .ToListAsync());
+        }
+    }
+}
